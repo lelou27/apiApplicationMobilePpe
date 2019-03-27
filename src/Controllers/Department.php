76acd414dcc8dbeb6f0ff368b799c994c41f04b1
lelou27 <lelou.pro@gmail.com>
2019-03-27 09:08:38 +0100

    develop department and praticien

--- src/Controllers/Department.php
+++ src/Controllers/Department.php
@@ -14,5 +14,45 @@ class Department {
         $this->db = $db;
     }
 
+    public function getAllDepartementsPraticien()
+    {
+        $db = $this->db;
+        /** @var $db PDO */
+
+        $query = "SELECT DISTINCT SUBSTR(PRA_CP, 1, 2) AS cp FROM praticien";
+
+        try {
+            $stmt = $db->prepare($query);
+            $stmt->exxecute();
+
+            $data = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (\Exception $e) {
+            $data = 'error';
+        }
+
+        return $data;
+    }
+
+    public function getPraticiensByDepartements($departement)
+    {
+        $db = $this->db;
+        /** @var $db PDO */
+
+        $departement = trim(strip_tags($departement));
+        $query = "SELECT * FROM praticien WHERE SUBSTR(PRA_CP, 1, 2) = :departement";
+
+        try {
+            $stmt = $db->prepare($query);
+            $stmt->bindValue(':departement', $departement, \PDO::PARAM_INT);
+            $stmt->execute();
+
+            $data = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (\Exception $e) {
+            $data = "Error";
+        }
+
+        return $data;
+    }
+
 
 }
--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,7 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 use App\Praticien;
+use App\Department;
 
 // Routes
 $app->get('/praticiens', function (Request $request, Response $response, array $args) {
@@ -14,7 +15,45 @@ $app->get('/praticiens', function (Request $request, Response $response, array $
 
         $response->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE));
     } catch (Exception $e) {
-        $response = 'ssks';
+        $response = $response->withJson([
+            'errorMess' => $e->getMessage(),
+            'errCode' => $e->getCode()
+        ]);
+    }
+
+    return $response;
+});
+
+$app->get('/departements', function (Request $request, Response $response, array $args) {
+    try {
+        $departement = new Department($this->db);
+
+        $data = $departement->getAllDepartementsPraticien();
+
+        $response->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE));
+    } catch (Exception $e) {
+        $response = $response->withJson([
+            'errorMess' => $e->getMessage(),
+            'errCode' => $e->getCode()
+        ]);
+    }
+
+    return $response;
+});
+
+$app->get('/departement/praticien/{id}', function (Request $request, Response $response, array $args) {
+    try {
+        $id = trim(strip_tags($args['id']));
+
+        $departement = new Department($this->db);
+        $datas = $departement->getPraticiensByDepartements(intval($id));
+
+        $response->getBody()->write(json_encode($datas, JSON_UNESCAPED_UNICODE));
+    } catch (Exception $e) {
+        $response = $response->withJson([
+            'errorMess' => $e->getMessage(),
+            'errCode' => $e->getCode()
+        ]);
     }
 
     return $response;
